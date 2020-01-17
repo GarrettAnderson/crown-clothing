fee08908cb7afda7b72c6000eafbaf18f7043940
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-01-17 13:29:44 -0500

    created sign in page and the sign in component. added route in order for user to navigate to the sign in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ShopPage from './pages/shop/ShopPage'
+import Header from './components/header/Header'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import history from './history'
 
 class App extends Component {
@@ -9,9 +11,11 @@ class App extends Component {
     return (
     <div>
       <Router history={history}>
+      <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </Router>
     </div>
